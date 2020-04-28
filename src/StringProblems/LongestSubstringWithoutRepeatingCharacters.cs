285c43fd230248c4634e07f48156fc5e5d39f1d7
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-04-28 23:48:46 +0530

    Added string problems project

---
+++ src/StringProblems/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringProblems
+{
+    /// <summary>
+    /// Problem is taken from GeeksForGeeks:
+    /// Length of the longest substring without repeating characters
+    /// https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+    /// </summary>
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int Find(string text)
+        {
+            var charDictionary = new Dictionary<char, int>
+            {
+                { text[0], 0 }
+            };
+            var currentLength = 1;
+            var maxLength = 1;
+
+            for (var i = 1; i < text.Length; i++)
+            {
+                if (charDictionary.ContainsKey(text[i]))
+                {
+                    var subStringIndex = i - currentLength;
+                    var previousIndex = charDictionary[text[i]];
+                    if (subStringIndex <= previousIndex)
+                    {
+                        currentLength = i - previousIndex;
+                    }
+                    else
+                    {
+                        currentLength++;
+                    }
+                    charDictionary[text[i]] = i;
+                }
+                else
+                {
+                    charDictionary.Add(text[i], i);
+                    currentLength++;
+                }
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
