3a215c14c3fa39dbe628d8d89e30e88234bb3b97
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-04-29 00:07:08 +0530

    Added new string problems

---
+++ src/StringProblems/DetermineIfStringHasAllUniqueCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems
+{
+    public class DetermineIfStringHasAllUniqueCharacters
+    {
+        public bool Check(string text)
+        {
+            var result = true;
+            var charDictionary = new Dictionary<char, int>();
+            foreach (var charItem in text)
+            {
+                if (charDictionary.ContainsKey(charItem))
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    charDictionary.Add(charItem, 1);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ test/StringProblems.Tests/DetermineIfStringHasAllUniqueCharactersTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems.Tests
+{
+    [TestClass]
+    public class DetermineIfStringHasAllUniqueCharactersTests
+    {
+        [DataTestMethod]
+        [DataRow("abc", true)]
+        [DataRow("aabc", false)]
+        [DataRow("xyzx", false)]
+        public void GivenText_Check_ReturnsBoolean(string text, bool expectedResult)
+        {
+            //Arrange
+            var problem = new DetermineIfStringHasAllUniqueCharacters();
+
+            //Act
+            var result = problem.Check(text);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
