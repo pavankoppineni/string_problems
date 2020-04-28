9a341d25ca635d6b4fcd0576a79cb46bb516a6d9
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-04-28 23:54:15 +0530

    Added longest sub string without repeating characters problem (#1)
    * Added string problems project
    
    * Update README.md

---
+++ test/StringProblems.Tests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringProblems.Tests
+{
+    [TestClass]
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+    }
+}
