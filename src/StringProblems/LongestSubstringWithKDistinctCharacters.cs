68244df63b7fe281740567224b17e18f40451af1
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-03 01:12:20 +0530

    Added new string problem (#3)
    * Added string problems project
    
    * Update README.md
    
    * Added new string problems
    
    * Added string problem
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/StringProblems/LongestSubstringWithKDistinctCharacters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StringProblems
+{
+    /// <summary>
+    /// Find the longest substring with k unique characters in a given string
+    /// https://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
+    /// </summary>
+    public class LongestSubstringWithKDistinctCharacters
+    {
+        public (int startIndex, int length) Find(string text, int k)
+        {
+            var currentIndex = 0;
+            var currentLength = 0;
+            var maxLength = 0;
+            var maxLengthIndex = 0;
+            var charDictionary = new Dictionary<char, int>();
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (charDictionary.ContainsKey(text[i]))
+                {
+                    charDictionary[text[i]] = i;
+                    currentLength++;
+                }
+                else
+                {
+                    if (charDictionary.Keys.Count == k)
+                    {
+                        KeyValuePair<char, int> leastIndex = charDictionary.First();
+                        foreach (var item in charDictionary)
+                        {
+                            if (item.Value < leastIndex.Value)
+                            {
+                                leastIndex = item;
+                            }
+                        }
+                        charDictionary.Remove(leastIndex.Key);
+                        currentIndex = leastIndex.Value + 1;
+                        charDictionary.Add(text[i], i);
+                        currentLength = i - currentIndex + 1;
+                    }
+                    else if (charDictionary.Keys.Count < k)
+                    {
+                        charDictionary.Add(text[i], i);
+                        currentLength++;
+                    }
+                }
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    maxLengthIndex = currentIndex;
+                }
+            }
+            return (maxLengthIndex, maxLength);
+        }
+    }
+}
---
+++ test/StringProblems.Tests/LongestSubstringWithKDistinctCharactersTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems.Tests
+{
+    [TestClass]
+    public class LongestSubstringWithKDistinctCharactersTests
+    {
+        public static IEnumerable<object[]> GetDataSet()
+        {
+            var dataSet = new List<object[]>();
+
+            dataSet.Add(new object[]
+            {
+                "aabbc", 2, (0, 4)
+            });
+            dataSet.Add(new object[]
+            {
+                "abc", 2, (0, 2)
+            });
+            dataSet.Add(new object[]
+            {
+                "aabbccccccd", 2, (2, 8)
+            });
+
+            return dataSet;
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetDataSet), DynamicDataSourceType.Method)]
+        public void GivenStringAndNumber_Find_ReturnsSubStringWithNDistinctCharacters(string text, int k, (int startIndex, int length) expectedResult)
+        {
+            //Arrange
+            var problem = new LongestSubstringWithKDistinctCharacters();
+
+            //Act
+            var result = problem.Find(text, k);
+
+            //Assert
+            Assert.AreEqual(expectedResult.startIndex, result.startIndex);
+            Assert.AreEqual(expectedResult.length, result.length);
+        }
+    }
+}
