a47947ce0996f5ad4d0d213717f22ae16be1bf15
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-05 22:46:38 +0530

    Added new problem (#5)
    * Added string problems project
    
    * Update README.md
    
    * Added new string problems
    
    * Added string problem
    
    * Added new string problem
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/StringProblems.Tests/CheckToSeeStringOneIsRotationOfAnotherStringTwoTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems.Tests
+{
+    [TestClass]
+    public class CheckToSeeStringOneIsRotationOfAnotherStringTwoTests
+    {
+        [DataTestMethod]
+        [DataRow("waterbottle", "bottlewater", true)]
+        [DataRow("pavankumar", "kumarpavan", true)]
+        [DataRow("hellotim", "timolleh", false)]
+        public void GiveTwoStrings_Find_ReturnsTrueIfStringTwoIsRotationOfStringOne(string str1, string str2, bool expectedResult)
+        {
+            //Arrange
+            var problem = new CheckToSeeStringOneIsRotationOfAnotherStringTwo();
+
+            //Act
+            var result = problem.Find(str1, str2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
