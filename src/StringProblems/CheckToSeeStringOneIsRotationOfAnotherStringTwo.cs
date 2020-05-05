ddf77c185cbd6c84e40f7e61d461dd999a90eedf
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-05-05 22:37:19 +0530

    Added new string problem

---
+++ src/StringProblems/CheckToSeeStringOneIsRotationOfAnotherStringTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems
+{
+    /// <summary>
+    /// Given two strings str1, str2.
+    /// Find out if str2 is rotation of str1.
+    /// Example : 
+    /// str1 = "waterbottle"
+    /// str2 = "erbottlewat"
+    /// 
+    /// str2 is rotation of str1
+    /// </summary>
+    public class CheckToSeeStringOneIsRotationOfAnotherStringTwo
+    {
+        public bool Find(string str1, string str2)
+        {
+            var str = str1 + str1;
+            return str.Contains(str2);
+        }
+    }
+}
