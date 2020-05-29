35cc197f64540c0402b52279f646436d1fdcdea4
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-29 17:20:49 +0530

    Added permutations of a string problem (#8)
    * Added new problem on string sorting
    
    * Added permutations of string
    
    * Update README.md
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/StringProblems/PermutationsOfString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+    /// </summary>
+    public class PermutationsOfString
+    {
+        public HashSet<string> GeneratePermutations(string str)
+        {
+            var items = new HashSet<string>();
+            Permutate(str, 0, str.Length - 1, items);
+            return items;
+        }
+
+        private void Permutate(string str, int low, int high, ISet<string> permutations)
+        {
+            if (low == high)
+            {
+                permutations.Add(str);
+                return;
+            }
+
+            for (var i = low; i <= high; i++)
+            {
+                str = Swap(str, low, i);
+                Permutate(str, low + 1, high, permutations);
+                str = Swap(str, low, i);
+            }
+        }
+
+        private string Swap(string str, int i, int j)
+        {
+            var charArray = str.ToCharArray();
+            var temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            return new string(charArray);
+        }
+    }
+}
---
+++ test/StringProblems.Tests/PermutationsOfStringTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringProblems.Tests
+{
+    [TestClass]
+    public class PermutationsOfStringTests
+    {
+        [TestMethod]
+        public void GivenString_Generate_ReturnsPermutationsOfString()
+        {
+            //Arrange
+            var str = "abc";
+            var permutationsOfString = new PermutationsOfString();
+            var expectedPermutations = new HashSet<string>();
+            expectedPermutations.Add("abc");
+            expectedPermutations.Add("acb");
+            expectedPermutations.Add("bac");
+            expectedPermutations.Add("bca");
+            expectedPermutations.Add("cab");
+            expectedPermutations.Add("cba");
+
+            //Act
+            var actualPermutations = permutationsOfString.GeneratePermutations(str);
+
+            //Assert
+            expectedPermutations.ExceptWith(actualPermutations);
+            Assert.AreEqual(0, expectedPermutations.Count);
+        }
+    }
+}
